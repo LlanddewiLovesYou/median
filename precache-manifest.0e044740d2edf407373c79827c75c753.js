291748e0c49abe67e34b3841c964c42232644405
Ian Del Duca <ianmdelduca@gmail.com>
2020-12-29 15:46:37 -0500

    Updates

--- precache-manifest.0e044740d2edf407373c79827c75c753.js
+++ precache-manifest.0e044740d2edf407373c79827c75c753.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85a76b7e2d5510b4bf64a4cf1a16e9c5",
+    "revision": "3f573ae29c9cd62a50c585f9e2725c8e",
     "url": "/median/index.html"
   },
   {
-    "revision": "1171f8aeb49e6dfd4004",
+    "revision": "6f1fd70779ef6d406036",
     "url": "/median/static/css/2.987b09ae.chunk.css"
   },
   {
-    "revision": "133c7d47bfa6808cf69b",
+    "revision": "1351962d4eb81f888c67",
     "url": "/median/static/css/main.f175793e.chunk.css"
   },
   {
-    "revision": "1171f8aeb49e6dfd4004",
-    "url": "/median/static/js/2.612b9241.chunk.js"
+    "revision": "6f1fd70779ef6d406036",
+    "url": "/median/static/js/2.aeeaf78f.chunk.js"
   },
   {
     "revision": "5d08152ca88aedf613d17ddb456c5b3b",
-    "url": "/median/static/js/2.612b9241.chunk.js.LICENSE.txt"
+    "url": "/median/static/js/2.aeeaf78f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "133c7d47bfa6808cf69b",
-    "url": "/median/static/js/main.d12db3fd.chunk.js"
+    "revision": "1351962d4eb81f888c67",
+    "url": "/median/static/js/main.7dd402e5.chunk.js"
   },
   {
     "revision": "f164c2471d191314fce8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/median/precache-manifest.27fbe6b68a54837a5b6649977dd9677f.js"
+  "/median/precache-manifest.0e044740d2edf407373c79827c75c753.js"
 );
 
 self.addEventListener('message', (event) => {
